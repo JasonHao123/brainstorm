63b070321566a8cfbec64ed1f1908aeeada8ea6a
Hao, Yanfeng <Yanfeng.Hao@sc.com>
2018-06-29 22:10:11 +0800

    added unit test case

--- ofbiz-product-component/src/test/java/jason/app/brainstorm/product/component/OfbizProductComponentTest.java
+++ ofbiz-product-component/src/test/java/jason/app/brainstorm/product/component/OfbizProductComponentTest.java
@@ -16,18 +16,50 @@
  */
 package jason.app.brainstorm.product.component;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.sql.DataSource;
+
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
-import org.apache.camel.test.junit4.CamelTestSupport;
+import org.apache.camel.model.dataformat.JsonLibrary;
+import org.apache.camel.test.spring.CamelSpringTestSupport;
+import org.dbunit.DataSourceDatabaseTester;
+import org.dbunit.IDatabaseTester;
+import org.dbunit.dataset.DataSetException;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 
-public class OfbizProductComponentTest extends CamelTestSupport {
-
+public class OfbizProductComponentTest extends CamelSpringTestSupport {
+	 private IDatabaseTester databaseTester;
+	
+	@Before
+	public void setup() throws Exception {
+		DataSource dataSource = (DataSource) applicationContext.getBean("dataSource");
+		databaseTester = new DataSourceDatabaseTester(dataSource);
+		IDataSet dataSet = new FlatXmlDataSetBuilder().build(new File("src/test/resources/dataset.xml"));
+        databaseTester.setDataSet( dataSet );
+	// will call default setUpOperation
+        databaseTester.onSetup();
+	}
+	
+	@After
+	public void tearDown() throws Exception
+    {
+	// will call default tearDownOperation
+        databaseTester.onTearDown();
+    }
     @Test
     public void testOfbizProduct() throws Exception {
         MockEndpoint mock = getMockEndpoint("mock:result");
         mock.expectedMinimumMessageCount(1);       
-        
+        mock.expectedBodiesReceived("{\"status\":0,\"categories\":[]}");
         assertMockEndpointsSatisfied();
     }
 
@@ -36,9 +68,18 @@ public class OfbizProductComponentTest extends CamelTestSupport {
         return new RouteBuilder() {
             public void configure() {
                 from("product://foo")
-                  .to("product://bar")
+                  .to("product://getCatalog")
+                  .marshal().json(JsonLibrary.Jackson)
+                  .to("log:test")
                   .to("mock:result");
             }
         };
     }
+
+	@Override
+	protected AbstractApplicationContext createApplicationContext() {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+		context.scan("jason.app.brainstorm.product");
+		return context;
+	}
 }
